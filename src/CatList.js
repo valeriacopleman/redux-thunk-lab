8ce5fc12f81ca0378294000d2ccbae79a251f474
Valeria Copleman <valeriacopleman@gmail.com>
2021-04-04 18:58:30 -0700

    Done.

--- src/CatList.js
+++ src/CatList.js
@@ -1,1 +1,19 @@
 // write your CatList component here
+import React, { Component } from 'react';
+//import { connect } from 'react-redux';
+
+class CatList extends Component {
+    renderCats = () => {
+        return this.props.catPics.map(cat => <img key={cat.id} src={cat.url} alt={''}/>)
+      }
+
+    render() {
+        return(
+            <div>
+                {this.renderCats()}
+            </div>
+        )
+    }
+}
+
+export default CatList;
